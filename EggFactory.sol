f8199bf6827780136c05092bdc11c89fe324c198
Ghulam Haider <haider.cs98@gmail.com>
2021-04-01 11:14:31 +0500

    added getters/setters security checks and event

--- EggFactory.sol
+++ EggFactory.sol
@@ -15,26 +15,85 @@ contract EggFactory {
     EggLootBox LOOTBOX;
     EggToken EGG_TOKEN;
     
-    constructor() {
-        CANDY_ERC20 = 0x1963E04a845213d005cDf0e22a39F85BD5941390;
-        LOOTBOX_OPTIONS = 4;
-        LOOTBOX_PRICES = [20, 50, 100];
-        LOOTBOX_PROBABILITIES = [
-                [60, 30, 10],
-                [30, 50, 20],
-                [10, 30, 60],
-                [ 5, 15, 80]
-            ];
-        EGG_OPTIONS = 20;
-        EGG_CLASSES = 3;
-        CLASS_COLORS = ["Red", "Green", "Blue"];
+    address factoryOwner;
+    
+    event EggLootBoxOpened(address opener, string mintedClass);
+    
+    constructor(
+            address rewardingAddress,
+            uint256 lootBoxOptions,
+            uint256[] memory lootBoxPrices,
+            uint256[][] memory lootBoxProbabilities,
+            uint256 eggOptions,
+            uint256 eggClasses,
+            string[] memory classColors
+        ) {
+        CANDY_ERC20 = rewardingAddress;
+        LOOTBOX_OPTIONS = lootBoxOptions;
+        LOOTBOX_PRICES = lootBoxPrices;
+        LOOTBOX_PROBABILITIES = lootBoxProbabilities;
+        EGG_OPTIONS = eggOptions;
+        EGG_CLASSES = eggClasses;
+        CLASS_COLORS = classColors;
         LOOTBOX = new EggLootBox(LOOTBOX_OPTIONS, EGG_OPTIONS, EGG_CLASSES);
         
         for(uint i = 0; i < LOOTBOX_OPTIONS; i++) {
-            LOOTBOX.setOptionClasses(i, LOOTBOX_PROBABILITIES[i]);
+            LOOTBOX.setEggOptionClasses(i, LOOTBOX_PROBABILITIES[i]);
         }
         
         EGG_TOKEN = new EggToken();
+        
+        factoryOwner = msg.sender;
+    }
+    
+    function setFactoryState(
+            address rewardingAddress,
+            uint256 lootBoxOptions,
+            uint256[] memory lootBoxPrices,
+            uint256[][] memory lootBoxProbabilities,
+            uint256 eggOptions,
+            uint256 eggClasses,
+            string[] memory classColors
+        ) external {
+        require(msg.sender == factoryOwner, "EggFactory::setFactoryState: Only owner can perform this operation");
+        CANDY_ERC20 = rewardingAddress;
+        LOOTBOX_OPTIONS = lootBoxOptions;
+        LOOTBOX_PRICES = lootBoxPrices;
+        LOOTBOX_PROBABILITIES = lootBoxProbabilities;
+        EGG_OPTIONS = eggOptions;
+        EGG_CLASSES = eggClasses;
+        CLASS_COLORS = classColors;
+        
+        LOOTBOX.setEggLootBoxState(LOOTBOX_OPTIONS, EGG_OPTIONS, EGG_CLASSES);
+        
+        for(uint i = 0; i < LOOTBOX_OPTIONS; i++) {
+            LOOTBOX.setEggOptionClasses(i, LOOTBOX_PROBABILITIES[i]);
+        }
+    }
+    
+    function getFactoryState() external view returns (
+            address,
+            uint256,
+            uint256[] memory,
+            uint256[][] memory,
+            uint256,
+            uint256,
+            string[] memory
+        ) {
+        return (
+            CANDY_ERC20,
+            LOOTBOX_OPTIONS,
+            LOOTBOX_PRICES,
+            LOOTBOX_PROBABILITIES,
+            EGG_OPTIONS,
+            EGG_CLASSES,
+            CLASS_COLORS
+        );
+    }
+    
+    function transferFactoryOwnership(address newOwner) external {
+        require(msg.sender == factoryOwner, "EggFactory::transferFactoryOwnership: Only owner can perform this operation");
+        factoryOwner = newOwner;
     }
     
     function buyLootBox(uint256 selectedOption) external {
@@ -45,5 +104,7 @@ contract EggFactory {
         //CandyToken._burn(msg.sender, LOOTBOX_PRICES[selectedOption]);
         
         EGG_TOKEN.mint(msg.sender, CLASS_COLORS[returnedClass]);
+        
+        emit EggLootBoxOpened(msg.sender, CLASS_COLORS[returnedClass]);
     }
 }
