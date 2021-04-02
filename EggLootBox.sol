b0241caffd7b46ee79bd8aa1b219cfa07fd079f6
Ghulam Haider <haider.cs98@gmail.com>
2021-04-03 02:54:20 +0500

    added pausable and burnable extensions

--- EggLootBox.sol
+++ EggLootBox.sol
@@ -6,22 +6,20 @@ contract EggLootBox is LootBoxRandomness {
     uint256 LOOTBOX_OPTIONS;
     mapping (uint256 => uint256[]) LOOTBOX_OPTION_PROBABILITIES;
     
-    constructor(uint256 lootboxOptions, uint256 _eggOptions, uint256 _eggClasses)
-    LootBoxRandomness(_eggOptions, _eggClasses)
+    constructor(uint256 lootboxOptions, uint256 _eggClasses)
+    LootBoxRandomness(_eggClasses)
     {
         LOOTBOX_OPTIONS = lootboxOptions;
     }
     
-    function setEggLootBoxState(uint256 newLootBoxOptions, uint256 newEggOptions, uint256 newEggClasses) public {
+    function setEggLootBoxState(uint256 newLootBoxOptions, uint256 newEggClasses) public {
         LOOTBOX_OPTIONS = newLootBoxOptions;
-        _setLootBoxState(newEggOptions, newEggClasses);
+        _setLootBoxState(newEggClasses);
     }
     
-    function getEggLootBoxState() public view returns(uint256, uint256, uint256) {
-        uint256 eggOptions;
-        uint256 eggClasses;
-        (eggOptions, eggClasses) = _getLootBoxState();
-        return (LOOTBOX_OPTIONS, eggOptions, eggClasses);
+    function getEggLootBoxState() public view returns(uint256, uint256) {
+        uint256 eggClasses  = _getLootBoxState();
+        return (LOOTBOX_OPTIONS, eggClasses);
     }
     
     function setEggOptionClasses(uint256 lootBox, uint256[] memory probabilities) public {
