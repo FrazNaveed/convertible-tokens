da9a407a33eab5c7dc861690175a5f5289e16130
Ghulam Haider <haider.cs98@gmail.com>
2021-04-08 18:25:21 +0500

    added ownership, access rights, burnable and pausable logics

--- EggToken.sol
+++ EggToken.sol
@@ -6,27 +6,53 @@ contract EggToken is ERC721Pausable {
     uint256 EGG_COUNTER;
     mapping (uint256 => string) EGG_COLORS;
     
+    address owner;
+    address eggFactory;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner || msg.sender == eggFactory, "EggToken: This action can only be performed by owner or EggFactory");
+        _;
+    }
+    
     constructor(
         string memory name_,
         string memory symbol_
         ) ERC721(name_, symbol_) {
         EGG_COUNTER = 0;
+        owner = msg.sender;
     }
     
-    function mint(address receiver, string memory color) public returns(uint256) {
-        // make it so only eggfactory can mint
+    function transferOwnership(address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+    
+    function setParentFactory(address newEggFactory) external onlyOwner {
+        eggFactory = newEggFactory;
+    }
+    
+    function getOwners() external view returns(address, address) {
+        return(owner, eggFactory);
+    }
+    
+    function getEggColor(uint256 eggId) external view returns(string memory) {
+        return EGG_COLORS[eggId];
+    }
+    
+    function mint(address receiver, string memory color) external onlyOwner returns(uint256) {
         _mint(receiver, ++EGG_COUNTER);
         EGG_COLORS[EGG_COUNTER] = color;
         return EGG_COUNTER;
     }
     
-    function getEggColor(uint256 eggId) public view returns(string memory) {
-        return EGG_COLORS[eggId];
+    function burn(uint256 tokenId) external onlyOwner {
+        _burn(tokenId);
     }
     
-    function burn(uint256 tokenId) public virtual {
-        // make it so only petfactory or msgsender can burn
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Burnable: caller is not owner nor approved");
-        _burn(tokenId);
+    function pause() external onlyOwner {
+        _pause();
+    }
+    
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
