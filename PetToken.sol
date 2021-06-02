715505c186fd80de5a6fb5c84bf96c7bd7fe0448
Haider Sattar <ryancit2@gmail.com>
2021-06-03 03:23:05 +0500

    refactor r4

--- PetToken.sol
+++ PetToken.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
 import "./deps/IPetToken.sol";
@@ -5,16 +6,13 @@ import "./deps/ERC721.sol";
 
 contract PetToken is ERC721, IPetToken {
     uint256 RANDOM_NONCE;
-    string[] PET_NAMES;
-    string[] PET_VISUALS;
-    
     uint256 MINTED_COUNT;
-    mapping(uint256 => uint256) MINTED_PETS; // uniqueToken => petId
+    mapping(uint256 => petMetadata) MINTED_PETS; // uniqueToken => petMetadata
     mapping(address => uint256[]) userBalance; // address => uniqueToken
-    
+
     address owner;
     address eggFactory;
-    
+
     modifier onlyOwner() {
         require(msg.sender == owner || msg.sender == eggFactory, "PetToken: This action can only be performed by owner or PetFactory");
         _;
@@ -38,50 +36,56 @@ contract PetToken is ERC721, IPetToken {
         return(owner, eggFactory);
     }
     
-    function addPet(string memory petName, string memory petVisual) external override onlyOwner {
-        PET_NAMES.push(petName);
-        PET_VISUALS.push(petVisual);
-    }
+    // function addPet(string memory petName, string memory petVisual) external override onlyOwner {
+    //     PET_NAMES.push(petName);
+    //     PET_VISUALS.push(petVisual);
+    // }
     
-    function updatePetData(uint256 id, string memory newName, string memory newVisual) external override onlyOwner {
-        PET_NAMES[id] = newName;
-        PET_VISUALS[id] = newVisual;
-    }
+    // function updatePetData(uint256 id, string memory newName, string memory newVisual) external override onlyOwner {
+    //     PET_NAMES[id] = newName;
+    //     PET_VISUALS[id] = newVisual;
+    // }
 
     function getUserPets(address user) external view override returns(uint256[] memory pets) {
         pets = userBalance[user];
     }
 
-    function getPetFromId(uint256 id) external view override returns(string memory name, string memory visual) {
-        name = PET_NAMES[id];
-        visual = PET_VISUALS[id];
-    }
+    // function getPetFromId(uint256 id) external view override returns(string memory name, string memory visual) {
+    //     name = PET_NAMES[id];
+    //     visual = PET_VISUALS[id];
+    // }
     
-    function getMintedPet(uint256 mintedId) external view override returns(string memory petName, string memory petVisual) {
-        uint256 petId = MINTED_PETS[mintedId];
-        petName = PET_NAMES[petId];
-        petVisual = PET_VISUALS[petId];
+    function getMintedPet(uint256 mintedId) external view override returns(petMetadata memory metadata) {
+        metadata = MINTED_PETS[mintedId];
     }
 
-    function getAllPets() external view override returns(string[] memory names, string[] memory visuals) {
-        names = PET_NAMES;
-        visuals = PET_VISUALS;
-    }
+    // function getAllPets() external view override returns(string[] memory names, string[] memory visuals) {
+    //     names = PET_NAMES;
+    //     visuals = PET_VISUALS;
+    // }
     
-    function mintRandom(address receiver, uint256[] memory classProbabilities) external override onlyOwner returns(uint256, string memory) {
-        uint256 randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, RANDOM_NONCE++))) % 100;
-        uint256 sumSoFar = 0;
-        uint256 selectedClass = 0;
-        for (uint256 i = 0; i < classProbabilities.length; i++) {
-            sumSoFar += classProbabilities[i];
-            if (randomNumber <= sumSoFar) {
-                selectedClass = i;
-                break;
-            }
-        }
+    // function mintRandom(address receiver, uint256[] memory classProbabilities) external override onlyOwner returns(uint256, string memory) {
+    //     uint256 randomNumber = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, RANDOM_NONCE++))) % 100;
+    //     uint256 sumSoFar = 0;
+    //     uint256 selectedClass = 0;
+    //     for (uint256 i = 0; i < classProbabilities.length; i++) {
+    //         sumSoFar += classProbabilities[i];
+    //         if (randomNumber <= sumSoFar) {
+    //             selectedClass = i;
+    //             break;
+    //         }
+    //     }
+    //     _mint(receiver, ++MINTED_COUNT);
+    //     MINTED_PETS[MINTED_COUNT] = selectedClass;
+    //     userBalance[receiver].push(MINTED_COUNT);
+    //     return (MINTED_COUNT, PET_NAMES[selectedClass]);
+    // }
+
+    function mintPet(address receiver, string memory name, string memory visual, uint256 hp, uint256 attack, uint256 defence, uint256 special1, uint256 special2) external override onlyOwner returns(uint256 mintedId) {
+        petMetadata memory pmd = petMetadata(name, visual, hp, attack, defence, special1, special2);
         _mint(receiver, ++MINTED_COUNT);
-        MINTED_PETS[MINTED_COUNT] = selectedClass;
+        MINTED_PETS[MINTED_COUNT] = pmd;
         userBalance[receiver].push(MINTED_COUNT);
-        return (MINTED_COUNT, PET_NAMES[selectedClass]);
+        return (MINTED_COUNT);
     }
 }
--- deps/IPetToken.sol
+++ deps/IPetToken.sol
@@ -1,23 +1,36 @@
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
 interface IPetToken {
+    struct petMetadata {
+        string name;
+        string visual;
+        uint256 hp;
+        uint256 attack;
+        uint256 defence;
+        uint256 special1;
+        uint256 special2;
+    }
+
     function transferOwnership(address) external;
     
     function setParentFactory(address) external;
     
     function getOwners() external view returns(address, address);
     
-    function addPet(string memory, string memory) external;
+    // function addPet(string memory, string memory) external;
     
-    function updatePetData(uint256, string memory, string memory) external;
+    // function updatePetData(uint256, string memory, string memory) external;
 
     function getUserPets(address) external view returns(uint256[] memory);
 
-    function getPetFromId(uint256) external view returns(string memory, string memory);
+    // function getPetFromId(uint256) external view returns(string memory, string memory);
     
-    function getMintedPet(uint256) external view returns(string memory, string memory);
+    function getMintedPet(uint256) external view returns(petMetadata memory);
     
-    function getAllPets() external view returns(string[] memory, string[] memory);
+    // function getAllPets() external view returns(string[] memory, string[] memory);
 
-    function mintRandom(address, uint256[] memory) external returns(uint256, string memory);
+    // function mintRandom(address, uint256[] memory) external returns(uint256, string memory);
+
+    function mintPet(address, string memory, string memory, uint256, uint256, uint256, uint256, uint256) external returns(uint256);
 }
